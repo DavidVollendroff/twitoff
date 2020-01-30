7add8d976d3d6e761805d6401b643bf72110dbb2
David Vollendroff <civilianemail@gmail.com>
2020-01-30 13:15:25 -0800

    Changed back import statements

--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,7 +1,7 @@
 import basilica
 import tweepy
 from decouple import config
-from models import User, DB, Tweet
+from .models import User, DB, Tweet
 
 TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'), config('TWITTER_CONSUMER_SECRET'))
 TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'), config('TWITTER_ACCESS_TOKEN_SECRET'))
